4335f115dc888473e9ff05274356b61a8cd3aecc
Mehmet Faruk Baran <mehmetfaruk@hiraparl.com>
2022-02-23 09:29:55 +0300

    Initial Commit

---
+++ src/main/java/com/example/demo/user/model/User.java
@@ -0,0 +1,90 @@
+package com.example.demo.user.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@Document
+public class User implements Serializable {
+
+    @Id
+    private String id;
+
+    @Field
+    private String username;
+
+    @Field
+    private String password;
+
+    @Field
+    private String fullName;
+
+    public User() {
+    }
+
+    public User(String id, String username, String password, String fullName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", fullName='" + fullName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(fullName, user.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, fullName);
+    }
+}
